8128f3e063b83ce05eba2135d88dee9bc73cbf50
Jacob Waterman <jacobrwaterman@gmail.com>
2015-01-10 22:50:49 -0600

    added AWS

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'bootstrap-sass'
 gem 'devise'
 gem 'paperclip', '~> 4.2'
+gem 'aws-sdk', '~> 1.20.0'
 
 group :development, :test do
 	gem 'sqlite3'
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,6 +1,6 @@
 class Pic < ActiveRecord::Base
 	belongs_to :admin
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :large => "600x600" }
          validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
