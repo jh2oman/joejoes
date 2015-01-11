c2fa29bcbea853637378377f3ca177882a5d93a1
Jacob Waterman <jacobrwaterman@gmail.com>
2015-01-11 00:37:58 -0600

    background photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def create
+def create
   @admin = Admin.create( admin_params )
 end
 
